fb8e5bdb3fa1b8cb0077f4d7f8d3c8c7ff811a5a
anastasiasakunova <sakunchik1@yandex.ru>
2018-12-16 12:01:51 +0300

    documents added

--- app/src/main/java/msk/android/academy/javatemplate/documents/DocumentData.kt
+++ app/src/main/java/msk/android/academy/javatemplate/documents/DocumentData.kt
@@ -1,6 +1,1 @@
-package msk.android.academy.javatemplate.documents
-
-import android.graphics.Bitmap
-
-data class DocumentData(var name : String, var image : Bitmap, var path : String)
-
+package msk.android.academy.javatemplate.documentsimport android.graphics.Bitmapdata class DocumentData(var name : String, var image : Bitmap, var path : String)
