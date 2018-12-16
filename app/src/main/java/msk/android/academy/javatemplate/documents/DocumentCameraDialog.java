8813c4cdfde8d7a073d4bfb5f91ce61e4f15d8b0
anastasiasakunova <sakunchik1@yandex.ru>
2018-12-16 12:03:12 +0300

    documents added

--- app/src/main/java/msk/android/academy/javatemplate/documents/DocumentCameraDialog.java
+++ app/src/main/java/msk/android/academy/javatemplate/documents/DocumentCameraDialog.java
@@ -1,57 +1,1 @@
-package msk.android.academy.javatemplate.documents;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.widget.EditText;
-
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-
-import msk.android.academy.javatemplate.R;
-import pl.aprilapps.easyphotopicker.EasyImage;
-
-public class DocumentCameraDialog extends DialogFragment {
-    private String inputText;
-    private EditText editText;
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-
-        Bundle args = getArguments();
-
-        builder.setView(inflater.inflate(R.layout.document_camera_dialog, null));
-        builder.setTitle("Добавление документа");
-        builder.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                editText = getDialog().findViewById(R.id.document_edit_text);
-                inputText = String.valueOf(editText.getText());
-
-                takePhoto();
-                Log.e("!!!", "Ввели: " + inputText);
-            }
-        });
-        builder.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dismiss();
-            }
-        });
-
-        return builder.create();
-    }
-
-    public String getInputText() {
-        return inputText;
-    }
-
-    private void takePhoto() {
-        EasyImage.openCameraForImage(getActivity(), 0);
-    }
-}
+package msk.android.academy.javatemplate.documents;import android.app.AlertDialog;import android.app.Dialog;import android.content.DialogInterface;import android.os.Bundle;import android.support.v4.app.DialogFragment;import android.util.Log;import android.view.LayoutInflater;import android.widget.EditText;import android.support.annotation.NonNull;import android.support.annotation.Nullable;import msk.android.academy.javatemplate.R;import pl.aprilapps.easyphotopicker.EasyImage;public class DocumentCameraDialog extends DialogFragment {    private String inputText;    private EditText editText;    @NonNull    @Override    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());        LayoutInflater inflater = getActivity().getLayoutInflater();        Bundle args = getArguments();        builder.setView(inflater.inflate(R.layout.document_camera_dialog, null));        builder.setTitle("Добавление документа");        builder.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {            public void onClick(DialogInterface dialog, int which) {                editText = getDialog().findViewById(R.id.document_edit_text);                inputText = String.valueOf(editText.getText());                takePhoto();                Log.e("!!!", "Ввели: " + inputText);            }        });        builder.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {            public void onClick(DialogInterface dialog, int which) {                dismiss();            }        });        return builder.create();    }    public String getInputText() {        return inputText;    }    private void takePhoto() {        EasyImage.openCameraForImage(getActivity(), 0);    }}
--- app/src/main/java/msk/android/academy/javatemplate/documents/DocumentFullPhotoFragment.java
+++ app/src/main/java/msk/android/academy/javatemplate/documents/DocumentFullPhotoFragment.java
@@ -1,47 +1,1 @@
-package msk.android.academy.javatemplate.documents;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import java.io.File;
-
-import msk.android.academy.javatemplate.R;
-
-public class DocumentFullPhotoFragment extends Fragment {
-    private ImageView imageView;
-
-    public static DocumentFullPhotoFragment newInstanse(String path) {
-        DocumentFullPhotoFragment documentFullPhotoFragment = new DocumentFullPhotoFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString("12311", path);
-        documentFullPhotoFragment.setArguments(bundle);
-        return documentFullPhotoFragment;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.document_full_photo_fragment, container, false);
-        imageView = view.findViewById(R.id.document_full_image_view);
-        if (getArguments() != null) {
-            String path = getArguments().getString("12311");
-            File imgFile = new  File(path);
-
-            if(imgFile.exists()){
-                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                imageView.setImageBitmap(DocumentFragment.createRotatedBitmap(myBitmap, path));
-            }
-        }
-        return view;
-    }
-
-
-}
+package msk.android.academy.javatemplate.documents;import android.graphics.Bitmap;import android.graphics.BitmapFactory;import android.os.Bundle;import android.support.annotation.NonNull;import android.support.annotation.Nullable;import android.support.v4.app.Fragment;import android.view.LayoutInflater;import android.view.View;import android.view.ViewGroup;import android.widget.ImageView;import java.io.File;import msk.android.academy.javatemplate.R;public class DocumentFullPhotoFragment extends Fragment {    private ImageView imageView;    public static DocumentFullPhotoFragment newInstanse(String path) {        DocumentFullPhotoFragment documentFullPhotoFragment = new DocumentFullPhotoFragment();        Bundle bundle = new Bundle();        bundle.putString("12311", path);        documentFullPhotoFragment.setArguments(bundle);        return documentFullPhotoFragment;    }    @Nullable    @Override    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {        View view = inflater.inflate(R.layout.document_full_photo_fragment, container, false);        imageView = view.findViewById(R.id.document_full_image_view);        if (getArguments() != null) {            String path = getArguments().getString("12311");            File imgFile = new  File(path);            if(imgFile.exists()){                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());                imageView.setImageBitmap(DocumentFragment.createRotatedBitmap(myBitmap, path));            }        }        return view;    }}
